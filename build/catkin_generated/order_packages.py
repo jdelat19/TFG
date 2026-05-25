e4a0245836d3b85a8a7223e143c1578b1976c389
jdelat19 <javierdelatorremorales@gmail.com>
2026-05-25 14:24:19 +0200

    Voz a emocion integrado pero falta mejorar

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/javier/Escritorio/TFG/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/javier/Escritorio/TFG/devel;/opt/ros/noetic'.split(';') if '/home/javier/Escritorio/TFG/devel;/opt/ros/noetic' != '' else []
+underlay_workspaces = '/home/javier/Escritorio/preVoice/TFG/devel;/home/javier/Escritorio/TFG/devel;/opt/ros/noetic'.split(';') if '/home/javier/Escritorio/preVoice/TFG/devel;/home/javier/Escritorio/TFG/devel;/opt/ros/noetic' != '' else []
--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,4 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export PWD='/home/javier/Escritorio/TFG/build'
+export PWD='/home/javier/Escritorio/preVoice/TFG/build'
